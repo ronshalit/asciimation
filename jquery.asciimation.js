8e40fcb8e59e29b600b17622481e2796378a663f
ronshalit <shalit.ron@gmail.com>
2013-11-14 10:56:12 +0200

    Added some documentation

--- jquery.asciimation.js
+++ jquery.asciimation.js
@@ -1,10 +1,15 @@
+/*
+  Author: Ron Shalit 2013
+  Purpose: show nice ascii animations instead of plain animated gifs that nobody likes
+*/
 (function () {
     jQuery.fn.asciimation = function (customOptions) {
         
         var _settings = {
-            frames: ['—', '\\', '|', '/'],
+            frames: ['â€•', '\\', '|', '/'],
             interval: 200
         }
+        
         function spinnerStart(that,options) {
             var i = 0;
             interval = setInterval(function () {
@@ -12,14 +17,18 @@
             }, options.interval);
             that.data("asciimation.interval", interval);
         }
-
+        
+        // if custom options were supplied, or no options at all, start spinning the animation
         if (typeof customOptions === 'object' || typeof customOptions=== 'undefined') {
             var options = $.extend(_settings, customOptions||{});
             spinnerStart(this, options);
         }
-
+        
+        // if the argument was "destroy", the stop the animation and clear the content
         if (customOptions === 'destroy') {
-            clearInterval(this.data("asciimation.interval"));
+            var int = this.data("asciimation.interval");
+            if(int)
+                clearInterval(int);
             this.html("");
         }
 
